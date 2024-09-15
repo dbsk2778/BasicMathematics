573c7158c865b5f7b0efa0fe91a1dfe51e607730
dbsk2778 <dbsk2778@gmail.com>
2024-09-15 20:35:22 +0900

    기초 수학 : 알고리즘 복잡도 정의 추가

---
+++ src/기초수학/알고리즘복잡도/Example.java
@@ -0,0 +1,18 @@
+package 기초수학.알고리즘복잡도;
+
+public class Example {
+    /*
+        코딩 테스트에서는 메모리는 비교적 자유롭고, 시간 복잡도를 줄여야할 필요성이 있음
+
+        1. 복잡도 (Complexity)
+        - 알고리즘 성능을 나타내는 척도
+
+        2. 시간 복잡도 (Time Complexity)
+        - 알고리즘의 필요 연산 횟수
+
+        3. 공간 복잡도 (Space Complexity)
+        - 알고리즘의 필요 메모리
+
+        * 시간 복잡도와 공간 복잡도는 Trade-off 관계
+     */
+}
